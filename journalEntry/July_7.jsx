08ad9f2c452eb39a5567bceeb0ca5cff88584aec
bheem-works <shokokomichan098@gmail.com>
2025-07-07 23:58:32 +0545

    journal of the 7 july

---
+++ journalEntry/July_7.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function July_7() {
+  return (
+    <div>
+    <div className="start">
+        Day - 7 july months year 2025; 
+        Today was a fine day and nice weather. I just spend my day by doing some time pass and being lazy. 
+        I have not waash my cloths and the many thing's and the clean thing's like i used to do the day before. 
+        But i am goint to all the sutff and going to be the responsible at the tuesday. 
+        
+    </div>
+     <div className="event">
+        I have learned many thing's and many of the thing's are over wheleming for me. 
+        Today was a fine day i learned and talk a lot with the persons and get the idea about it. 
+        I have also made the decisioson to do my task and be motivated and also learn the new thing's. 
+        I will accept my failure and mistakes and learn from it. I would also accept i am a beginner and 
+        don't know many thing's right now.So, i am goint to learn the many thing's today.
+        Today was a greate day. I learned about like the difference between the js and teh c. and the difference 
+        between the compiler and the engine. That's it for the today Event. 
+     </div>
+    </div>
+  )
+}
