b312ab047dde78bbd241da1ab2ba26905f2bc1c8
bheem-works <shokokomichan098@gmail.com>
2025-07-09 23:25:22 +0545

    Event complete

--- journalEntry/july_nine.jsx
+++ journalEntry/july_nine.jsx
@@ -8,7 +8,33 @@ function july_nine() {
         I was expecting that my friend would come today at the college and that's why i also i have attended the college. 
         But unfortounately he has not come. And yeh that's it. And i have not speaked to much at the college. 
         I have set the boundries to speak how much i have to speak. I need to be quite and i have to speak when, 
-        I really need to speak. When some one is really listening to me. 
+        I really need to speak. When some one is really listening to me. I have write the how much words do i need to speak. 
+        Like i have write some thing like this M-20; It means that i need to speak just the 20 words. And yeh i think i have 
+        done that succesfully. 
+      </div>
+      <br /><br /> 
+      {/* gap */}
+
+
+      <div className="event">
+      <p>
+        Event * Nothing's speciall happend i just set the boundries and limit. And i must have to set the boundries. 
+        To gain the control over my self. And funny thing's i have sleeped so much time. And it seems like i have to 
+        <b>Awake all night!Today. </b> And i have decided to write my bad habits and the good habits and what i am afraid of. 
+        And about the relations ship it is just like the thing's what it would be. It is like the kabhi kushi kabhi kaam. 
+        She is very important and the unique for me. And the unique thing's don't get easily. I have to suffer for her. 
+        And the thing's and i commit with my self is that. I have must have to admit for that what i have. 
+        And i must have to being honest with my self and with my person. I have to be free soon. And i know i will. 
+        I must have to understand the thing's and fact. I have to also write some books with my self. And yeh as always.
+        Whenever i listne the audio summary the gigls it just give me so much happy. Unfourtanetly i don't have money to 
+        buy the premium ships or you can the bank account to buy it otherwise i would buy it and listen all the books and note 
+        down the all the steps and implement that in the real life. The gigl and the audio books has teached me lot. <br />
+        <br />
+        And i am greatful about that thing's. Thanks Gigl. And proudly i can say i am a gigler. That's it for today. 
+        I have to learn and works. And keep my hopes up and do what i want and stay being happy what i do. 
+        I would never lie to my self and i would be honest with self and 
+        i would also accept the reality and the thign's what happened with me all the time, and ucoming future.
+        </p>  
       </div>
     </div>
   )
