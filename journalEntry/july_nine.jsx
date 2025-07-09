a10fd36ab2b77b7ebc7dc28f3c29db243033df13
bheem-works <shokokomichan098@gmail.com>
2025-07-09 23:14:07 +0545

    Starting commit

---
+++ journalEntry/july_nine.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function july_nine() {
+  return (
+    <div>
+      <div className="start">
+        Today moroning was so sweaty and the hot. You can say my hopes and the expectations gets crushed at the morning. 
+        I was expecting that my friend would come today at the college and that's why i also i have attended the college. 
+        But unfortounately he has not come. And yeh that's it. And i have not speaked to much at the college. 
+        I have set the boundries to speak how much i have to speak. I need to be quite and i have to speak when, 
+        I really need to speak. When some one is really listening to me. 
+      </div>
+    </div>
+  )
+}
+
+export default july_nine
