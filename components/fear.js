7298d94847f379663562a0770227c97ca45eb652
bheem-works <shokokomichan098@gmail.com>
2025-07-12 22:12:08 +0545

    starting journaling

---
+++ components/fear.js
@@ -0,0 +1,51 @@
+// today i want to talk about my fear.
+//
+// So what are my fears and why i am so afraid of it. i know writting in this note. This will not remove my fear.
+// But i want to talk about it and i want to write about the fears. So, what is it and why i am so afraid of it.
+// So, we all do have the fear in our life.
+//
+// Here is  the list of my fear -;
+//
+// 1.To lose my passions(computer stuff)
+// 2. To lose my person family (love)
+// 3. To be late. can not grow with them timely;
+//      These are the main steps that i always scare to lose.
+// 4. lose my self. lose my favourite thing's persons(miso)
+// 5. lose the thing's what i have got.
+// 6. fear about they will not wait for me. My love will not wait for me. if a took a long time.
+// 7. feeling like i am the only who understand her and love her.
+// 8. fear about am i going to make her alone soon?
+// 9. fear what i have dreamed that can not be able to do like as -> (author,singer,editor,enterprinuer,understandin dad)
+// 10. fear she will take me with her where she will go and think me like as choice.
+// 11. fear about those people who believe me am i going to broke their trust?
+// 12. fear about when i lose everything's i will die.
+// 13. fear about when i earn to much. I wil be changed.
+// 14. fear about when i get so much rich i will kills peoples.
+// 15. fear about how people gonna reject me at anything's.
+// 16. fear about not going to get a job;
+// 17. fear about not going to get a proper thing's and the proper want what i want.
+// 18. fear abotu people are going to cheat on me.
+// 19. fear about what if i repeat the mistakes. do the people who i love do they forgive?
+// 20. fear abut i want the multiple thing's can i be able to achieve that all.
+// 21. fear about can i cheat my girl in the future.
+// 22. fear about lossing interest and some love againsts my girls.
+// 23. fear about how i think and how i love do my person love me like that way.
+// 24. fear about i have many hopes and the expectation and the negative points against my love.
+// 25. fear abotu can i live with her. having a lack of trust .
+// 26. fear about what if she do that mistakes again what should i do? should i leave her.
+// 27. fear abotu i speak many wrong thing's that hurt's people.
+// 28. fear about what am i, and what i can be in the future. is my people gonna accept me; ?
+// 29. fear about do i deserve this love and what love am i having am i going to loose it?
+// 30. fear about am i going to be cold. fear about what if i have to live alone.
+// 31. feara about  why persons don't care about my self (including her also);
+// 32. fear about why my girls goes to the another man?
+// 33. fear  about what if i go to the another woman?
+//
+// So these, are stupid fear i have talk about to main fear in the beginning. and ther can be some of the overthinkig.
+// Stuff also... I did not know how to connect this thing's. hear got scared to much time because i love some one.
+// And the way i am getting love that scared me. the way she told me leave me that words hurt me. fear about the mistakes
+// what happened in my previous past and the thing's what other girls says to me. am i going to loose her. i don't want to lie
+// her but yeh i had a true love. and that was my first love. and yeh i just give my full of the ting's to her.
+// may be there can be many mistakes i have done in that time. but i learn from it. and yeh the way i love my girls is this
+// a true love or the attachment. you can not get it what is attachment and the what is love coz you never know about this stuff.
+// until you realize with your self. and when you realise it is too late.
---
+++ journalEntry/july_12.jsx
@@ -0,0 +1,21 @@
+import "react" from 'React';
+
+function July_12() {
+  return(
+    <div className="container">
+      <div className="start">
+      <p>So today was a good day perhpas i just read some few documentation and made a promies with my self. 
+      But i have not done that thing's yet. so let's talk about the whole over today thing's. and i will write
+      the thing's what i have make the commitement and the other's important stuff in the event. 
+      so today morning was pretty good and everything's went so good. i enjoyed my time and read the documentation and
+      yeh i just talk in english with my lady and happily. I think i am doing well and i am giving her
+      encourage to do something's and yeh. i just went to the samdhi house. aas always i keep taking far away the 
+      studay thing's and not even reading and not even taking the notes. Look how lazy i am. haha
+      Can not even open the desktop and solve it. assss me and my behave at some times. 
+    </p>
+      </div>
+    </div>
+  )
+}
+
+export default July_12;
